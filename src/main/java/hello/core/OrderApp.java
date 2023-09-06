e38c8fb65c2db3430d8ef9bd1a031c4eb4c9c8b0
thom-window <hsbang.thom@gmail.com>
2023-09-06 11:12:55 +0900

    refactor: changed to annotation configured application context

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -5,14 +5,20 @@ import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-        OrderService orderService = appConfig.orderService();
-        MemberService memberService= appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        OrderService orderService = appConfig.orderService();
+//        MemberService memberService= appConfig.memberService();
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = ctx.getBean("memberService", MemberService.class);
+        OrderService orderService = ctx.getBean("orderService", OrderService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
